3a07c7f2e87d5565835d4f316fa94ec80f06e885
RayDev1988 <devsops2022@gmail.com>
2022-07-22 18:10:28 -0700

    fetch token every 20 seconds

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/header"
 import TokenDetails from "./components/TokenDetails";
 import { fetchTokenBalances } from "./utils";
-import DarkMode from "./DarkMode";
 
 function App() {
 
   const [balances, setBalances] = useState([]);
   const [address, setAddress] = useState("");
+  const [timerId, setTimerId] = useState(-1);
 
   useEffect(() => {
     const storageItem = localStorage.getItem("address");
     if(storageItem) {
       setAddress(storageItem);
       fetchBalances(storageItem);
+      fetchByTime(storageItem);
     }
   }, []);
+
+  const fetchByTime = (address) => {
+    if(timerId !== -1) {
+      window.clearInterval(timerId);
+    }
+    const tid = window.setInterval(fetchBalances, 20000, address);
+    setTimerId(tid);
+  }
 
   const fetchBalances = async (address) => {
     const data = await fetchTokenBalances(address);
@@ -29,6 +38,7 @@ function App() {
 
   const findTokenBlanace = () => {
     localStorage.setItem("address", address);
+    fetchByTime(address);
     fetchBalances(address);
   }
 
